c713a8ff8c968cd602949c76cac27ae2c4cefdef
Bai Chan Kheo <kheob@hotmail.com>
2016-03-09 03:32:38 +1100

    Commit

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,20 @@
-angular.module("todoListApp", []);
+angular.module("todoListApp", [])
 //Second parameter defines the application's dependencies
 //Must include [] so angular knows to create the app
     //Otherwise, it looks for the app instead of creating it
+
+.controller('mainCtrl', function($scope) {
+    $scope.helloWorld = function() {
+        console.log("Hello there! This is the helloWorld controller function.");
+    };
+
+    $scope.todos = [
+        {"name": "clean the house"},
+        {"name": "water the dog"},
+        {"name": "feed the lawn"},
+        {"name": "pay dem bills"},
+        {"name": "run"},
+        {"name": "swim"}
+    ]
+});
+//mainCtrl's scope is inside the element it's attached to (ng-controller)
